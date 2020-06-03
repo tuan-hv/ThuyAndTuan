e5593527aa4a83f17ea08b0950f49312a4786d1b
tuanhv2 <tuanhv2@smartosc.com>
2020-06-03 12:32:00 +0700

    update moduls

---
+++ service-order/src/main/java/com/serviceorder/serviceorder/entity/Orders.java
@@ -0,0 +1,37 @@
+package com.serviceorder.serviceorder.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Orders implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int orderId;
+    @Column(name = "totalprice", precision = 20, scale = 3)
+    private double totalPrice;
+    @CreationTimestamp
+    @Column(name = "createdat", updatable = false)
+    private Date createdAt;
+    @UpdateTimestamp
+    @Column(name = "updatedat")
+    private Date updatedAt;
+    @Column(name = "status")
+    private int status;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "orders")
+    private List<OrderDetail> orderDetailEntities;
+
+}
