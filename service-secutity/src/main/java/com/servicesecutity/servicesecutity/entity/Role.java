c27c90446bc4f0befefa9ff210e7577bd4923786
tuanhv2 <tuanhv2@smartosc.com>
2020-06-03 10:52:10 +0700

    add authentication service

---
+++ service-secutity/src/main/java/com/servicesecutity/servicesecutity/entity/Role.java
@@ -0,0 +1,28 @@
+package com.servicesecutity.servicesecutity.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.NaturalId;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
+@Data
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @NaturalId
+    @Column(length = 60)
+    private RoleName name;
+
+    public Role() {
+    }
+
+    public Role(RoleName name) {
+        this.name = name;
+    }
+
+}
