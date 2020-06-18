7b33081caf45277b422ad9cc9f69d122b8181fbe
tuanhv2 <tuanhv2@smartosc.com>
2020-06-18 16:11:25 +0700

    fix code

--- payment/src/main/java/com/payment/swagger/SwaggerRedirectController.java
+++ payment/src/main/java/com/payment/swagger/SwaggerRedirectController.java
@@ -1,14 +1,13 @@
 package com.payment.swagger;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 class SwaggerRedirectController {
 
-	@RequestMapping(value = "/api/payment", method = RequestMethod.GET)
+	@GetMapping(value = "/api/payment")
 	public ModelAndView redirect1() {
 		return new ModelAndView("redirect:/swagger-ui.html");
 	}
